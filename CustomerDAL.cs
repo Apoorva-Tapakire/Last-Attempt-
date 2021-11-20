36322679eb878a58fc2959b44285dea23d001c3f
Apoorva-Tapakire <72929437+Apoorva-Tapakire@users.noreply.github.com>
2021-11-20 18:48:57 +0530

    DAL

---
+++ CustomerDAL.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BalLibrary;
+
+
+namespace DalLibrary
+{
+    public class CustomerDAL
+    {
+        public string AddCustomer(CustomerBAL ba)
+        {
+            SqlConnection cn = new SqlConnection("server=LAPTOP-6C58L34N\\SQLEXPRESS;Integrated Security=true;database=OnlineShopping");
+            string s = "[dbo].[p_InsertCustomer]";
+            SqlCommand cmd = new SqlCommand(s, cn);
+            cmd.Parameters.AddWithValue("@p_firstname", ba.FirstName);
+            cmd.Parameters.AddWithValue("@p_lastname", ba.LastName);
+            cmd.Parameters.AddWithValue("@p_emailId", ba.EmailId);
+            cmd.Parameters.AddWithValue("@p_password", ba.Password);
+            cmd.Parameters.AddWithValue("@p_MobileNos", ba.MobileNo);
+            cmd.Parameters.AddWithValue("@p_city", ba.City);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cn.Open();
+            string s1 = "u are welcome";
+
+            cmd.ExecuteNonQuery();
+            cn.Close();
+            cn.Dispose();
+            return s1;
+        }
+
+
+        public string AddToCart(CartBAL bal)
+        {
+            SqlConnection cn = new SqlConnection("server=LAPTOP-6C58L34N\\SQLEXPRESS;Integrated Security=true;database=OnlineShopping");
+            string s = "[dbo].[p_InsertCart]";
+            SqlCommand cmd = new SqlCommand(s, cn);
+            cmd.Parameters.AddWithValue("@quantity", bal.Quantity);
+            cmd.Parameters.AddWithValue("@productId", bal.ProductId);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cn.Open();
+            string s1 = "Items have been added to the string";
+            cmd.ExecuteNonQuery();
+            cn.Close();
+            cn.Dispose();
+            return s1;
+        }
+
+
+        public string UpdateCart(CartBAL bal)
+        {
+            SqlConnection cn = new SqlConnection("server=LAPTOP-6C58L34N\\SQLEXPRESS;Integrated Security=true;database=OnlineShopping");
+            string s = "[dbo].[p_UpdateCart]";
+            SqlCommand cmd = new SqlCommand(s, cn);
+            cmd.Parameters.AddWithValue("@quantity", bal.Quantity);
+            cmd.Parameters.AddWithValue("@productId", bal.ProductId);
+
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cn.Open();
+            string s1 = "Items have been updated";
+
+            cmd.ExecuteNonQuery();
+            cn.Close();
+            cn.Dispose();
+            return s1;
+        }
+
+
+
+        public string DeleteCart(CartBAL bal)
+        {
+            SqlConnection cn = new SqlConnection("server=LAPTOP-6C58L34N\\SQLEXPRESS;Integrated Security=true;database=OnlineShopping");
+            string s = "[dbo].[sp_DeleteCartItem] ";
+            SqlCommand cmd = new SqlCommand(s, cn);
+
+            cmd.Parameters.AddWithValue("@productId", bal.ProductId);
+
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cn.Open();
+            string s1 = "Item has been deleted";
+
+            cmd.ExecuteNonQuery();
+            cn.Close();
+            cn.Dispose();
+            return s1;
+        }
+    }
+}
---
+++ PaymentDAL.cs
@@ -0,0 +1,32 @@
+﻿using BalLibrary;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DalLibrary
+{
+    public class PaymentDAL
+    {
+        public void PaymentMethod(PaymentBAL bal)
+        {
+            SqlConnection cn = new SqlConnection("Data Source=LAPTOP-6C58L34N\\SQLEXPRESS;Integrated Security=true;Initial Catalog=OnlineShopping");
+            string s = "[dbo].[Payment_insert]";
+            SqlCommand cmd = new SqlCommand(s, cn);
+            cmd.Parameters.AddWithValue("@p_BankName", bal.BankName);
+            cmd.Parameters.AddWithValue("@p_ISFCCode", bal.IFSCCode);
+            cmd.Parameters.AddWithValue("@p_CardNo", bal.CardNo);
+            cmd.Parameters.AddWithValue("@p_CVVNo", bal.CVVNo);
+            cmd.Parameters.AddWithValue("@p_UPi", bal.UPINo);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cn.Open();
+            cmd.ExecuteNonQuery();
+
+            cn.Close();
+            cn.Dispose();
+        }
+    }
+}
