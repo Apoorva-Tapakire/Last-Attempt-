42b5d3522fa97808679334393ab90a0078bad281
Apoorva-Tapakire <72929437+Apoorva-Tapakire@users.noreply.github.com>
2021-11-20 18:49:54 +0530

    Controllers

---
+++ CustomerController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BalLibrary;
+using DalLibrary;
+using System.Web.Http.Cors;
+using ProjectWebApplication.Models;
+
+
+namespace ProjectWebApplication.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class CustomerController : ApiController
+    {
+        public string Get()
+        {
+            return "value";
+        }
+        
+
+
+        [HttpPost]
+        public IHttpActionResult CustomerInsert(CustomerModel cust)
+        {
+            CustomerDAL ds = new CustomerDAL();
+            CustomerBAL ba = new CustomerBAL();
+
+            ba.EmailId = cust.EmailId;
+            ba.Password = cust.Password;
+            ba.FirstName = cust.FirstName;
+            ba.LastName = cust.LastName;
+            ba.MobileNo = cust.MobileNo;
+            ba.City = cust.City;
+
+            string s1 = ds.AddCustomer(ba);
+            return Ok(new { status = 200, isSuccess = true, message = "ur logged sucess", });
+
+        }
+
+
+        [HttpPost]
+        public IHttpActionResult CartInsert(CartModel cart)
+        {
+            CustomerDAL ds = new CustomerDAL();
+            CartBAL bal = new CartBAL();
+
+            bal.Quantity = cart.Quantity;
+            bal.ProductId = cart.ProductId;
+
+
+            string s1 = ds.AddToCart(bal);
+            return Ok(new { status = 200, isSuccess = true, message = "ur logged sucess", });
+
+        }
+
+        [HttpPut]
+        public IHttpActionResult CartUpdate(CartModel cart)
+        {
+            CustomerDAL ds = new CustomerDAL();
+            CartBAL bal = new CartBAL();
+
+            bal.Quantity = cart.Quantity;
+            bal.ProductId = cart.ProductId;
+
+
+            string s1 = ds.UpdateCart(bal);
+            return Ok(new { status = 200, isSuccess = true, message = "ur logged sucess", });
+
+        }
+
+
+        [HttpPost]
+
+        public IHttpActionResult CartDelete(CartModel cart)
+        {
+            CustomerDAL ds = new CustomerDAL();
+            CartBAL bal = new CartBAL();
+            bal.ProductId = cart.ProductId;
+
+
+            string s1 = ds.DeleteCart(bal);
+            return Ok(new { status = 200, isSuccess = true, message = "ur product is deleted", });
+
+        }
+    }
+}
+
+
+
+
---
+++ PaymentController.cs
@@ -0,0 +1,32 @@
+﻿using BalLibrary;
+using DalLibrary;
+using ProjectWebApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace ProjectWebApplication.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class PaymentController : ApiController
+    {
+
+        [HttpPost]
+        public IHttpActionResult PaymentAdd(PaymentModel pay)
+        {
+            PaymentBAL bal = new PaymentBAL();
+            bal.CardNo = pay.CardNo;
+            bal.BankName = pay.BankName;
+            bal.CVVNo = pay.CVVNo;
+            bal.IFSCCode = pay.IFSCCode;
+            bal.UPINo = pay.UPINo;
+            PaymentDAL dal = new PaymentDAL();
+            dal.PaymentMethod(bal);
+            return Ok(new { status = 200, isSuccess = true, message = "valid user" });
+        }
+    }
+}
