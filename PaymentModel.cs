8ee20cf07ab625bc97fa410bad32c3ebc2fd5d2b
Apoorva-Tapakire <72929437+Apoorva-Tapakire@users.noreply.github.com>
2021-11-20 18:50:37 +0530

    Models

---
+++ PaymentModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectWebApplication.Models
+{
+    public class PaymentModel
+    {
+        public string BankName { get; set; }
+        public string IFSCCode { get; set; }
+        public string UPINo { get; set; }
+        public long CardNo { get; set; }
+        public long CVVNo { get; set; }
+    }
+}
